ac0635e1d9d1fa5b47bab7c8435cff85657a3d3c
ganeshssawant <99634208+ganeshssawant@users.noreply.github.com>
2022-02-18 20:34:04 +0530

    Update model.py

--- db/model.py
+++ db/model.py
@@ -1,8 +1,4 @@
-#from sqlalchemy import Column, Integer, String, ForeignKey, Index
-#from sqlalchemy.orm import relationship
-#from flask_login import UserMixin 
-#from app import application, db # ignore
-
+#from enum import unique
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,69 +6,67 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///noduesapp.sqlite"
 db = SQLAlchemy(app)
 
-#def getForeignKey(reference) : 
-#	return Column(db.BigInteger().with_variant(db.Integer, "sqlite"), ForeignKey(reference), nullable=False)
-
-class Records(db.Model) : 
+class Rcords(db.Model) : 
 	__tablename__ = 'Records'
 
 	#Creation of attributes in tables
 
-	id = Column(db.Integer(), primary_key= True)
+	id          = db.Column(db.Column, primary_key= True)
+	Account_id  = db.Column(db.Integer,Foreignkey('Account.id'))
+	Datatype_id = db.Column(db.Integer,Foreignkey('Datatype.id'))
+	App_id      = db.Column(db.Integer,Foreignkey('app.id'))
+	Detect_id   = db.Column(db.Integer,Foreignkey('detect.id'))
 
-    
-	Account_id = Column(db.BigInteger().with_variant(db.Integer, "sqlite"),Foreignkey('Account.id'))
-	Datatype_id = Column(db.BigInteger().with_variant(db.Integer, "sqlite"),Foreignkey('Datatype.id'))
-	App_id =Column(db.BigInteger().with_variant(db.Integer, "sqlite"),Foreignkey('app.id'))
-	Detect_id = Column(db.BigInteger().with_variant(db.Integer, "sqlite"),Foreignkey('detect.id'))
-
-
-    Detect=relationship("Detect",back_populate="Records")
-    Datattype=relationship("Dataeype",back_populate="Records")
-    App=relationship("App",back_populate="Records")
-    
     
 class Account_info(db.Model):
     __tablename__ = 'Account_info'
 
     #Creation of attributes in Account_info
 
-    Fullname=Column(String(255), nullable=False, unique=True)
-    Gender=Column(String(255), nullable=False, unique=True)
-    Ages=Column(db.integer, nullable=False, unique=True)
-    email = Column(String(255), nullable=False, unique=True)
-    Username=Column(String(255), nullable=False, unique=True)
-    password = Column(String(255), nullable=False)
-    Account_id=Column(db.BigInteger().with_variant(db.Integer, "sqlite"), primary_key= True)
+    Fullname   = db.Column(String(255), nullable=False, unique=True)
+    Gender     = db.Column(String(255), nullable=False, unique=True)
+    Ages       = db.Column(db.integer, nullable=False, unique=True)
+    email      = db.Column(String(255), nullable=False, unique=True)
+    Username   = db.Column(String(255), nullable=False, unique=True)
+    password   = db.Column(String(255), nullable=False)
+    Account_id = db.Column(db.Integer, primary_key= True)
 
-    # creating class constructor to store values in database
-
-    def __init__(self, Fullname, Gender, email, Username,password) : 
-        self.Fullname = Fullname
-        self.Gender = Gender
-        self.email = email
-        self.Username=Username
-        self.password = password
-
+    Account_record = db.relationship('Accountrecord',backref='Account_record',lazy=True)
 
 class Detect(db.Model):
     __tablename__ = 'Detect'
 
-    detct_id=Column(db.integer, nullable=False, unique=True)
-    Result=Column(String(255), nullable=False, unique=True)
+    detct_id = db.Column(db.integer, nullable=False, unique=True)
+    Result   = db.Column(String(255), nullable=False, unique=True)
 
-    Records=relationship("Records",back_populate="Detect")
+    Account_detect = db.relationship('Accountdetect',backref='Account_detect',lazy=True)
+
 
 class Datatype(db.Model):
     __tablename__ = 'Datatype'
 
-    id=Column(db.integer, nullable=False, unique=True)
-    Result=Column(String(255), nullable=False, unique=True)
+    id     = db.Column(db.integer, nullable=False, unique=True)
+    Result = db.Column(String(255), nullable=False, unique=True)
 
-    Records=relationship("Records",back_populate="Datatype")
+    Account_datatype = db.relationship('Accountdatatype',backref='Account_datatype',lazy=True)
 
 class App(db.Model):
     __tablename__ = 'App'
 
-    id=Column(db.integer, nullable=False, unique=True)
-    Result=Column(String(255), nullable=False, unique=True)
+    id     =db.Column(db.integer, nullable=False, unique=True)
+    Result =db.Column(String(255), nullable=False, unique=True)
+   
+    Account_app = db.relationship('Accountapp',backref='Account_app',lazy=True)
+
+
+
+
+
+    
+
+
+    
+    
+
+
+   
