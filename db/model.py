0a6a09d0ddb0f09d07fee839841bb4656794fcb4
lokeshparab <86286632+lokeshparab-pvppcoe@users.noreply.github.com>
2022-02-19 12:32:57 +0530

    db model fixed

--- db/model.py
+++ db/model.py
@@ -1,72 +1,70 @@
 #from enum import unique
+#from datetime import date
+#from enum import unique
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///noduesapp.sqlite"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.sqlite"
 db = SQLAlchemy(app)
 
-class Rcords(db.Model) : 
-	__tablename__ = 'Records'
+class Records(db.Model) : 
+    __tablename__ = 'Records'
 
-	#Creation of attributes in tables
+    #Creation of attributes in tables
 
-	id          = db.Column(db.Column, primary_key= True)
-	Account_id  = db.Column(db.Integer,Foreignkey('Account.id'))
-	Datatype_id = db.Column(db.Integer,Foreignkey('Datatype.id'))
-	App_id      = db.Column(db.Integer,Foreignkey('app.id'))
-	Detect_id   = db.Column(db.Integer,Foreignkey('detect.id'))
+    id          = db.Column(db.Integer, primary_key= True)
+
+    data        = db.Column(db.String(700), nullable= False)
+    date        = db.Column(db.Datetime, nullable= False)
+
+    account_id  = db.Column(db.Integer,db.Foreignkey('Account.id'), nullable=False)
+    datatype_id = db.Column(db.Integer,db.Foreignkey('Datatype.id'), nullable=False)
+    app_id      = db.Column(db.Integer,db.Foreignkey('app.id'), nullable=True)
+    detect_id   = db.Column(db.Integer,db.Foreignkey('detect.id'), nullable=False)
 
     
-class Account_info(db.Model):
-    __tablename__ = 'Account_info'
+class Account(db.Model):
+    __tablename__ = 'Account'
 
     #Creation of attributes in Account_info
+    account_id = db.Column(db.Integer, primary_key= True )
 
-    Fullname   = db.Column(String(255), nullable=False, unique=True)
-    Gender     = db.Column(String(255), nullable=False, unique=True)
-    Ages       = db.Column(db.integer, nullable=False, unique=True)
-    email      = db.Column(String(255), nullable=False, unique=True)
-    Username   = db.Column(String(255), nullable=False, unique=True)
-    password   = db.Column(String(255), nullable=False)
-    Account_id = db.Column(db.Integer, primary_key= True)
+    fname       = db.Column(db.String(128), nullable=False )
+    gender     = db.Column(db.String(7), nullable=False )
+    ages       = db.Column(db.Integer, nullable=False )
+    email      = db.Column(db.String(40), nullable=False, unique=True)
+    username   = db.Column(db.String(40), nullable=False, unique=True)
+    password   = db.Column(db.String(128), nullable=False)
+    
 
-    Account_record = db.relationship('Accountrecord',backref='Account_record',lazy=True)
+    account_record = db.relationship('Records',backref='record',lazy=True)
 
 class Detect(db.Model):
     __tablename__ = 'Detect'
 
-    detct_id = db.Column(db.integer, nullable=False, unique=True)
-    Result   = db.Column(String(255), nullable=False, unique=True)
+    detect_id = db.Column(db.Integer, primary_key= True)
 
-    Account_detect = db.relationship('Accountdetect',backref='Account_detect',lazy=True)
+    result   = db.Column(db.String(4), nullable=False, unique=True)
+
+    account_detect = db.relationship('Records',backref='detect',lazy=True)
 
 
 class Datatype(db.Model):
     __tablename__ = 'Datatype'
 
-    id     = db.Column(db.integer, nullable=False, unique=True)
-    Result = db.Column(String(255), nullable=False, unique=True)
+    id                  = db.Column(db.Integer,  primary_key= True)
 
-    Account_datatype = db.relationship('Accountdatatype',backref='Account_datatype',lazy=True)
+    datatype            = db.Column(db.String(7), nullable=False, unique=True)
+
+    account_datatype    = db.relationship('Records',backref='datatype',lazy=True)
 
 class App(db.Model):
     __tablename__ = 'App'
 
-    id     =db.Column(db.integer, nullable=False, unique=True)
-    Result =db.Column(String(255), nullable=False, unique=True)
+    id      = db.Column(db.Integer,  primary_key= True)
+
+    app     = db.Column(db.String(10), nullable=False, unique=True)
    
-    Account_app = db.relationship('Accountapp',backref='Account_app',lazy=True)
-
-
-
-
-
-    
-
-
-    
-    
-
-
-   
+    account_app = db.relationship('Records',backref='app',lazy=True)
+  
